7590866ff9c9a254014dcbd9cedf7f56c367f427
Sandra Naim <sandra.naim@toothpickapp.com>
2021-12-29 16:18:38 +0200

    create mock fetching functions

--- script2.js
+++ script2.js
@@ -4,25 +4,21 @@ const getPeoplePromise = fetch => {
     return fetch('https://swapi.dev/api/people')
         .then(response => response.json())
         .then(data => {
-            // console.log(data)
             return {
                 count: data.count,
                 results: data.results
             }
         })
 };
-// getPeoplePromise(fetch);
 
 const getPeopleAwait = async (fetch) => {
     const getRequest = await fetch('https://swapi.dev/api/people')
     const data = await getRequest.json()
-    // console.log(data)
     return {
         count: data.count,
         results: data.results
     }
 };
-// getPeopleAwait(fetch);
 
 module.exports = {
     getPeoplePromise,
