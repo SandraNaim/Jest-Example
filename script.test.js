1b2ebe723b2ddc22eaaa75e4165ce7b53143dd39
Sandra Naim <sandra.naim@toothpickapp.com>
2021-12-29 13:23:47 +0200

    add more tests and group all of them under one describe

--- script.test.js
+++ script.test.js
@@ -7,7 +7,18 @@ dbMock = [
     'dogpicture'
 ];
 
-it('is searching google', () => {
-    expect(googleSearch('testest', dbMock)).toEqual([])
-    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpicture'])
+describe('googleSearch', () => {
+    it('is searching google', () => {
+        expect(googleSearch('testest', dbMock)).toEqual([]);
+        expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpicture']);
+    })
+    
+    it('work with undefined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([]);
+    })
+    
+    it('does not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3);
+    })
 })
